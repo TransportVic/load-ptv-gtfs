ab78158f80d030924d13635a8b490aade5ff2cb2
Eyeball Edward <edwardyeung39@gmail.com>
2024-07-15 12:26:55 +1000

    download the gtfs file

--- test/ptv-gtfs.test.mjs
+++ test/ptv-gtfs.test.mjs
@@ -1,6 +1,13 @@
 import { expect } from 'chai'
 import nock from 'nock'
 import PTVGTFS from '../lib/gtfs-package/PTVGTFS.mjs'
+import path from 'path'
+import url from 'url'
+import { dir as tmpdir } from 'tmp-promise'
+import fs from 'fs/promises'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 let gtfsHost = 'http://data.ptv.vic.gov.au'
 let gtfsURL = '/downloads/gtfs.zip'
@@ -11,6 +18,8 @@ let responseHeaders = {
   'content-type': 'application/zip'
 }
 
+const stubGTFSUnzipping = (await fs.readFile(path.join(__dirname, 'sample-data', 'gtfs-unzipping', 'gtfs.zip')))
+
 describe('The PTVGTFS class', () => {
   describe('The getPublishedDate function', () => {
     it('Should read the last-modified header and return that', async () => {
@@ -20,6 +29,21 @@ describe('The PTVGTFS class', () => {
       let publishedDate = await gtfs.getPublishedDate()
 
       expect(publishedDate.toISOString()).to.equal('2024-07-12T02:25:21.000Z')
+    })
+  })
+
+  describe('The download function', () => {
+    it('Should save the gtfs file to the given folder, with the filename as gtfs.zip', async () => {
+      nock(gtfsHost).get(gtfsURL).reply(200, stubGTFSUnzipping)
+      let tmp = await tmpdir({ unsafeCleanup: true })
+
+      let gtfs = new PTVGTFS(gtfsHost + gtfsURL)
+      await gtfs.download(tmp.path)
+
+      let stat = await fs.stat(path.join(tmp.path, 'gtfs.zip'))
+      expect(stat.size).to.equal(23408)
+
+      await tmp.cleanup()
     })
   })
 })
