b04af1742b6179e2de4f6f6944e4e271bcc08eb4
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-13 19:36:33 +1100

    added test coverage for metro and regional route GTFS ID formats

--- test/gtfs-routes.test.mjs
+++ test/gtfs-routes.test.mjs
@@ -6,7 +6,8 @@ import url from 'url'
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const routesFile = path.join(__dirname, 'sample-data', 'routes', 'regional_bus_routes.txt')
+const regionalRoutesFile = path.join(__dirname, 'sample-data', 'routes', 'regional_bus_routes.txt')
+const metroRoutesFile = path.join(__dirname, 'sample-data', 'routes', 'metro_bus_routes.txt')
 
 let regionalBusRoute = {
   route_id: '6-10x-mjp-1',
@@ -29,16 +30,47 @@ describe('The GTFSRouteReader class', () => {
 
   describe('The getNextRoute function', () => {
     it('Should read the CSV file and return the next stop', async () => {
-      let routeReader = new GTFSRouteReader(routesFile)
+      let routeReader = new GTFSRouteReader(regionalRoutesFile)
       await routeReader.open()
 
       await routeReader.getNextRoute()
       let routeData = await routeReader.getNextRoute()
 
-      expect(routeData.routeGTFSID).to.equal('6-013')
       expect(routeData.agencyID).to.equal('99')
       expect(routeData.routeNumber).to.equal('13')
       expect(routeData.routeName).to.equal('Paynesville - Bairnsdale Via Paynesville')
+    })
+  })
+})
+
+describe('The GTFSRoute class', () => {
+  describe('The route ID parsing', () => {
+    it('Should parse a regular route ID and remove the -mjp- portion', async () => {
+      let routeReader = new GTFSRouteReader(regionalRoutesFile)
+      await routeReader.open()
+
+      let routeData = await routeReader.getNextRoute()
+      
+      expect(routeData.routeGTFSID).to.equal('6-10x')
+    })
+
+    it('Should parse a regular route ID and pad with 0s as needed', async () => {
+      let routeReader = new GTFSRouteReader(regionalRoutesFile)
+      await routeReader.open()
+
+      await routeReader.getNextRoute()
+      let routeData = await routeReader.getNextRoute()
+      
+      expect(routeData.routeGTFSID).to.equal('6-013')
+    })
+
+    it('Should detect a smartrak route ID and convert to a legacy 4-XXX trip', async () => {
+      let routeReader = new GTFSRouteReader(metroRoutesFile)
+      await routeReader.open()
+
+      expect((await routeReader.getNextRoute()).routeGTFSID).to.equal('4-900')
+      expect((await routeReader.getNextRoute()).routeGTFSID).to.equal('4-900')
+      expect((await routeReader.getNextRoute()).routeGTFSID).to.equal('4-601')
     })
   })
 })
