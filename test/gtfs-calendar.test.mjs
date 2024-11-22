5b147a163eae0950284f18fbe1a810114dc1bb15
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-22 22:05:20 +1100

    allow for adding/removing calendar dates

--- test/gtfs-calendar.test.mjs
+++ test/gtfs-calendar.test.mjs
@@ -1,9 +1,10 @@
 import { expect } from 'chai'
 import GTFSCalendarReader from '../lib/gtfs-parser/readers/GTFSCalendarReader.mjs'
-import GTFScalendar from '../lib/gtfs-parser/GTFSCalendar.mjs'
+import GTFSCalendar from '../lib/gtfs-parser/GTFSCalendar.mjs'
 import GTFSCalendarDate from '../lib/gtfs-parser/GTFSCalendarDate.mjs'
 import path from 'path'
 import url from 'url'
+import { CALENDAR_DATES } from '../lib/constants.mjs'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,7 +13,7 @@ const calendarFile = path.join(__dirname, 'sample-data', 'calendar', 'calendar.t
 
 describe('The GTFSCalendar class', () => {
   it('Should process the calendar data to a list of operation days', () => {
-    expect(new GTFScalendar({
+    expect(new GTFSCalendar({
       id: 'T1',
       startDay: '20241122',
       endDay: '20241129',
@@ -21,7 +22,7 @@ describe('The GTFSCalendar class', () => {
       "20241122", "20241123", "20241129"
     ])
 
-    expect(new GTFScalendar({
+    expect(new GTFSCalendar({
       id: 'T2',
       startDay: '20241123',
       endDay: '20241130',
@@ -32,7 +33,7 @@ describe('The GTFSCalendar class', () => {
   })
 
   it('Be inclusive of the end date', () => {
-    expect(new GTFScalendar({
+    expect(new GTFSCalendar({
       id: 'T1',
       startDay: '20241122',
       endDay: '20241122',
@@ -40,5 +41,53 @@ describe('The GTFSCalendar class', () => {
     }).getOperationDays()).to.have.members([
       "20241122"
     ])
+  })
+
+  it('Should allow adding of an extra date into the calendar', () => {
+    let cal = new GTFSCalendar({
+      id: 'T1',
+      startDay: '20241122',
+      endDay: '20241122',
+      daysOfWeek: ["1","1","1","1","1","1","1"]
+    })
+    
+    cal.processCalendarDate(new GTFSCalendarDate({
+      id: 'T1',
+      date: '20241125',
+      type: CALENDAR_DATES.ADDED
+    }))
+
+    expect(cal.getOperationDays()).to.have.members([
+      "20241122", "20241125"
+    ])
+  })
+
+  it('Should allow removing of a specific date into the calendar', () => {
+    let cal = new GTFSCalendar({
+      id: 'T1',
+      startDay: '20241224',
+      endDay: '20241226',
+      daysOfWeek: ["1","1","1","1","1","1","1"]
+    })
+    
+    cal.processCalendarDate(new GTFSCalendarDate({
+      id: 'T1',
+      date: '20241225',
+      type: CALENDAR_DATES.REMOVED
+    }))
+
+    expect(cal.getOperationDays()).to.deep.equal([
+      "20241224", "20241226"
+    ])
+  })
+})
+
+describe('The GTFSCalendarDate class', () => {
+  it('Should convert the CSV data into a proper date object', () => {
+    expect(new GTFSCalendarDate({
+      id: 'T6',
+      date: '20241125',
+      type: CALENDAR_DATES.ADDED
+    }).date.toUTC().toISO()).to.equal('2024-11-24T13:00:00.000Z')
   })
 })
