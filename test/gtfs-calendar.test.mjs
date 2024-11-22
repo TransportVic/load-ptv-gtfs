250333435d096ca1ab966ce66a9cbb1137e7e05d
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-22 21:05:00 +1100

    added tests

--- test/gtfs-calendar.test.mjs
+++ test/gtfs-calendar.test.mjs
@@ -11,14 +11,23 @@ const __dirname = path.dirname(__filename)
 const calendarFile = path.join(__dirname, 'sample-data', 'calendar', 'calendar.txt')
 
 describe('The GTFSCalendar class', () => {
-  it('Should accept the data fields given in the GTFS data', () => {
-    let calendar = new GTFScalendar({
+  it('Should process the calendar data to a list of operation days', () => {
+    expect(new GTFScalendar({
       id: 'T1',
       startDay: '20241122',
       endDay: '20241129',
       daysOfWeek: ["0","0","0","0","1","1","0"]
-    })
+    }).operationDays).to.have.members([
+      "20241122", "20241123", "20241129"
+    ])
 
-    
+    expect(new GTFScalendar({
+      id: 'T2',
+      startDay: '20241123',
+      endDay: '20241130',
+      daysOfWeek: ["1","1","1","1","1","0","0"]
+    }).operationDays).to.have.members([
+      "20241125", "20241126", "20241127", "20241128", "20241129"
+    ])
   })
 })
