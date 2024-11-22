3b32e2da2a181f47f4efab79a725e0d3318720fb
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-22 21:15:13 +1100

    added util class for parsing dates

---
+++ test/utils-test.mjs
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import { parseDate } from '../lib/utils.mjs'
+
+describe('The utils class', () => {
+  describe('The parseDate function', () => {
+    it('Should convert a GTFS date to a Melbourne timezoned date', () => {
+      expect(parseDate('20241122').toISOString()).to.equal('2024-11-21T13:00:00.000Z')
+    })
+  })
+})
