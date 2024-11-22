71b67f7705f4f42afc041a3a3d4bc3b8874a6777
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-22 21:33:17 +1100

    added a simple parseDate function

--- test/utils-test.mjs
+++ test/utils-test.mjs
@@ -4,7 +4,7 @@ import { parseDate } from '../lib/utils.mjs'
 describe('The utils class', () => {
   describe('The parseDate function', () => {
     it('Should convert a GTFS date to a Melbourne timezoned date', () => {
-      expect(parseDate('20241122').toISOString()).to.equal('2024-11-21T13:00:00.000Z')
+      expect(parseDate('20241122').toUTC().toISO()).to.equal('2024-11-21T13:00:00.000Z')
     })
   })
 })
