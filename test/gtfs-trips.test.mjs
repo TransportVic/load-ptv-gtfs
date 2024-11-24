ca44ea4b900a2bbeab975ab782ec6f0af5f51508
Eyeball Edward <edwardyeung39@gmail.com>
2024-11-24 14:43:24 +1100

    test for trip reader

--- test/gtfs-trips.test.mjs
+++ test/gtfs-trips.test.mjs
@@ -5,9 +5,12 @@ import GTFSCalendar from '../lib/gtfs-parser/GTFSCalendar.mjs'
 import GTFSRoute from '../lib/gtfs-parser/GTFSRoute.mjs'
 import { TRANSIT_MODES } from '../lib/constants.mjs'
 import GTFSTrip from '../lib/gtfs-parser/GTFSTrip.mjs'
+import GTFSTripReader from '../lib/gtfs-parser/readers/GTFSTripReader.mjs'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+const tripsFile = path.join(__dirname, 'sample-data', 'trips', 'trips.txt')
 
 describe('The GTFSTrip class', () => {
   it('Should process the skeleton trip data and populate the operation days', () => {
@@ -33,5 +36,28 @@ describe('The GTFSTrip class', () => {
     expect(trip.operationDays).to.have.members([
       '20241122', '20241123', '20241129'
     ])
+  })
+})
+
+describe('The GTFSTripReader class', () => {
+  it('Should read the trips.txt file and return a GTFSTrip object', async () => {
+    let calendars = {
+      'T2_2': new GTFSCalendar({
+        id: 'T2_2',
+        startDay: '20241122',
+        endDay: '20241122',
+        daysOfWeek: ["0","0","0","0","1","0","0"]
+      })
+    }
+
+    let reader = new GTFSTripReader(tripsFile)
+    await reader.open()
+
+    let trip = await reader.getNextEntity()
+
+    expect(trip.tripID).to.equal('1.T2.2-ALM-vpt-1.1.R')
+    expect(trip.routeID).to.equal('2-ALM-vpt-1')
+    expect(trip.headsign).to.equal('Camberwell')
+    expect(trip.operationDays).to.deep.equal(['20241122'])
   })
 })
