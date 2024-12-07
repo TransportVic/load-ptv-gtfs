0b5a278bf5ef90387c26a074b22d22ff82bb6ddf
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-07 15:57:22 +1100

    added shape reader

---
+++ test/gtfs-shapes.test.mjs
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import path from 'path'
+import url from 'url'
+import GTFSShapeReader from '../lib/gtfs-parser/readers/GTFSShapeReader.mjs'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const shapeFile = path.join(__dirname, 'sample-data', 'shapes', 'shapes.txt')
+
+describe('The GTFSShapeReader class', () => {
+  it('Should read the trips.txt file and return a GTFSTrip object', async () => {
+    let reader = new GTFSShapeReader(shapeFile)
+    await reader.open()
+
+    let shape = await reader.getNextEntity()
+
+    expect(shape.shapeID).to.equal('49-601-aus-1.2.R')
+    expect(shape.points.length).to.equal(46)
+    expect(shape.shapeLength).to.equal(3069.43)
+  })
+})
