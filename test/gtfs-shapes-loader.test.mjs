c3d89a3578db47abbf2b90cc38ad3b19dd111d9c
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-11 12:00:33 +1100

    test duplicate shape merging

--- test/gtfs-shapes-loader.test.mjs
+++ test/gtfs-shapes-loader.test.mjs
@@ -47,15 +47,54 @@ describe('The ShapeLoader class', () => {
     let shapeLoader = new ShapeLoader(shapeFile, database)
 
     await shapeLoader.loadShapes({ shapeIDMap })
-    
+
     let route = await routes.findDocument({ routeGTFSID: '4-601' })
 
     expect(route).to.exist
     expect(route.routePath.length).to.equal(2)
-    
+
     let monashHuntingdale = route.routePath.find(path => path.fullGTFSIDs.includes('49-601-aus-1.3.H'))
     expect(monashHuntingdale).to.exist
     expect(monashHuntingdale.length).to.equal(2848.26)
     expect(monashHuntingdale.path.coordinates.length).to.equal(38)
+  })
+
+  it('Should merge duplicate shapes with the same length and start/end coordinates', async () => {
+    let database = new LokiDatabaseConnection('test-db')
+    let stops = await database.createCollection('stops')
+    let routes = await database.createCollection('routes')
+    let trips = await database.createCollection('gtfs timetables')
+
+    let stopLoader = new StopsLoader(stopsFile, TRANSIT_MODES.metroBus, database)
+    await stopLoader.loadStops()
+
+    let routeLoader = new RouteLoader(routesFile, agencyFile, TRANSIT_MODES.metroBus, database)
+    await routeLoader.loadRoutes()
+
+    let routeIDMap = routeLoader.getRouteIDMap()
+
+    let tripLoader = new TripLoader({
+      tripsFile, stopTimesFile,
+      calendarFile, calendarDatesFile
+    }, TRANSIT_MODES.metroBus, database)
+
+    let shapeIDMap = await tripLoader.loadTrips({ routeIDMap })
+
+    let shapeLoader = new ShapeLoader(shapeFile, database)
+
+    await shapeLoader.loadShapes({ shapeIDMap })
+
+    let testTrip = await trips.findDocument({ tripID: '49-601--1-MF10-999' })
+    expect(testTrip.shapeID).to.equal('49-601-aus-1.4.H')
+
+    let route = await routes.findDocument({ routeGTFSID: testTrip.routeGTFSID })
+    expect(route.routeNumber).to.equal('601')
+
+    let tripShape = route.routePath.find(path => path.fullGTFSIDs.includes(testTrip.shapeID))
+    expect(tripShape).to.exist
+    expect(tripShape.fullGTFSIDs).to.have.members([
+      '49-601-aus-1.3.H',
+      '49-601-aus-1.4.H'
+    ])
   })
 })
