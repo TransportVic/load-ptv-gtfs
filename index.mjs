d93c65cddbcbd01b59814860add1a9f3e2cda56b
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-29 14:35:36 +1100

    expose gtfs types

--- index.mjs
+++ index.mjs
@@ -3,7 +3,24 @@ import ShapeLoader from './lib/loader/ShapeLoader.mjs'
 import StopsLoader from './lib/loader/StopsLoader.mjs'
 import TripLoader from './lib/loader/TripLoader.mjs'
 
+import GTFSAgency from './lib/gtfs-parser/GTFSAgency.mjs'
+import GTFSCalendar from './lib/gtfs-parser/GTFSCalendar.mjs'
+import GTFSCalendarDate from './lib/gtfs-parser/GTFSCalendarDate.mjs'
+import GTFSRoute from './lib/gtfs-parser/GTFSRoute.mjs'
+import { GTFSShape, GTFSShapePoint } from './lib/gtfs-parser/GTFSShape.mjs'
+import { GTFSStop, GTFSPlatformStop } from './lib/gtfs-parser/GTFSStop.mjs'
+import { GTFSStopTime } from './lib/gtfs-parser/GTFSStopTime.mjs'
+import GTFSTrip from './lib/gtfs-parser/GTFSTrip.mjs'
+
+const GTFSTypes = {
+  GTFSAgency, GTFSCalendar, GTFSCalendarDate,
+  GTFSRoute, GTFSShape, GTFSShapePoint,
+  GTFSStop, GTFSPlatformStop,
+  GTFSStopTime, GTFSTrip
+}
+
 export {
   RouteLoader, ShapeLoader,
-  StopsLoader, TripLoader
+  StopsLoader, TripLoader,
+  GTFSTypes
 }
