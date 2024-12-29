8ca1b025fdb4c8154beb74bfdf90d48ed1e1b252
Eyeball Edward <edwardyeung39@gmail.com>
2024-12-29 14:32:40 +1100

    added in documentation

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import RouteLoader from './lib/loader/RouteLoader.mjs'
+import ShapeLoader from './lib/loader/ShapeLoader.mjs'
+import StopsLoader from './lib/loader/StopsLoader.mjs'
+import TripLoader from './lib/loader/TripLoader.mjs'
+
+export {
+  RouteLoader, ShapeLoader,
+  StopsLoader, TripLoader
+}
