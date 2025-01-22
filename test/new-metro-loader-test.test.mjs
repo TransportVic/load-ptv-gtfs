85cc6cecb587bc0dd435020be13962ae754fa281
Eyeball Edward <edwardyeung39@gmail.com>
2025-01-22 21:23:53 +1100

    reorganised files and move new metro tests to a new file

--- test/new-metro-loader-test.test.mjs
+++ test/new-metro-loader-test.test.mjs
@@ -9,6 +9,9 @@ import suburbs from './sample-data/suburbs.json' with { type: 'json' }
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const newStopTimesFile = path.join(__dirname, 'sample-data', 'trips', 'stop_times_new.txt')
+const newTripsFile = path.join(__dirname, 'sample-data', 'trips', 'trips_new.txt')
+
 const stopsFile = path.join(__dirname, 'sample-data', 'gtfs-splitting', 'nov_2024_train_data.txt')
 
 describe('The GTFS Stops Loader (Testing on the projected Nov 2024 data)', () => {
@@ -46,4 +49,31 @@ describe('The GTFS Stops Loader (Testing on the projected Nov 2024 data)', () =>
       expect(platforms[i].platform).to.equal((i + 1).toString())
     }
   })
+})
+
+it('It should accept trip IDs in the new format and extract the TDN from there', async () => {
+  let database = new LokiDatabaseConnection('test-db')
+  let stops = await database.createCollection('stops')
+  let routes = await database.createCollection('routes')
+  let trips = await database.createCollection('gtfs timetables')
+
+  let stopLoader = new StopsLoader(stopsFile, suburbs, TRANSIT_MODES.metroTrain, database)
+  await stopLoader.loadStops()
+
+  let routeLoader = new RouteLoader(routesFile, agencyFile, TRANSIT_MODES.metroTrain, database)
+  await routeLoader.loadRoutes()
+
+  let routeIDMap = routeLoader.getRouteIDMap()
+
+  let tripLoader = new TripLoader({
+    tripsFile: newTripsFile, stopTimesFile: newStopTimesFile,
+    calendarFile, calendarDatesFile
+  }, TRANSIT_MODES.metroTrain, database)
+  
+  await tripLoader.loadTrips({ routeIDMap })
+
+  let trip = await trips.findDocument({ tripID: '02-ALM--1-T2-2302' })
+
+  expect(trip).to.not.be.null
+  expect(trip.runID).to.equal('2302')
 })
