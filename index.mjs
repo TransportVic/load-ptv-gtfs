7973eda33b02af9c11aa345a3398c855a91976c8
Eyeball Edward <edwardyeung39@gmail.com>
2025-01-26 09:33:09 +1100

    expose reader classes

--- index.mjs
+++ index.mjs
@@ -15,6 +15,15 @@ import GTFSTrip from './lib/gtfs-parser/GTFSTrip.mjs'
 import setRouteStops from './lib/post-processing/set-route-stops.mjs'
 import setStopServices from './lib/post-processing/set-stop-services.mjs'
 
+import GTFSAgencyReader from './lib/gtfs-parser/readers/GTFSAgencyReader.mjs'
+import GTFSCalendarDateReader from './lib/gtfs-parser/readers/GTFSCalendarDatesReader.mjs'
+import GTFSCalendarReader from './lib/gtfs-parser/readers/GTFSCalendarReader.mjs'
+import GTFSRouteReader from './lib/gtfs-parser/readers/GTFSRouteReader.mjs'
+import GTFSShapeReader from './lib/gtfs-parser/readers/GTFSShapeReader.mjs'
+import GTFSStopsReader from './lib/gtfs-parser/readers/GTFSStopsReader.mjs'
+import GTFSStopTimesReader from './lib/gtfs-parser/readers/GTFSStopTimesReader.mjs'
+import GTFSTripReader from './lib/gtfs-parser/readers/GTFSTripReader.mjs'
+
 const GTFSTypes = {
   GTFSAgency, GTFSCalendar, GTFSCalendarDate,
   GTFSRoute, GTFSShape, GTFSShapePoint,
@@ -22,9 +31,15 @@ const GTFSTypes = {
   GTFSStopTime, GTFSTrip
 }
 
+const GTFSReaders = {
+  GTFSAgencyReader, GTFSCalendarDateReader, GTFSCalendarReader,
+  GTFSRouteReader, GTFSShapeReader, GTFSStopsReader, 
+  GTFSStopTimesReader, GTFSTripReader
+}
+
 export {
   RouteLoader, ShapeLoader,
   StopsLoader, TripLoader,
-  GTFSTypes,
+  GTFSTypes, GTFSReaders,
   setRouteStops, setStopServices
 }
