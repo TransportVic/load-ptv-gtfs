26a76dfab3e5f9b5b73beffdaf0e339f5e38ff89
Eyeball Edward <edwardyeung39@gmail.com>
2025-05-22 22:44:47 +1000

    added new 23 may stop format data

---
+++ test/newer-tram-format.test.mjs
@@ -0,0 +1,35 @@
+import { LokiDatabaseConnection } from '@transportme/database'
+import StopsLoader from '../lib/loader/StopsLoader.mjs'
+import { TRANSIT_MODES } from '../lib/constants.mjs'
+import path from 'path'
+import url from 'url'
+import { expect } from 'chai'
+import suburbs from './sample-data/suburbs.json' with { type: 'json' }
+import RouteLoader from '../lib/loader/RouteLoader.mjs'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const agencyFile = path.join(__dirname, 'sample-data', 'routes', 'agency.txt')
+
+const routesFile = path.join(__dirname, 'sample-data', 'new-tram-2', 'routes.txt')
+const stopsFile = path.join(__dirname, 'sample-data', 'new-tram-2', 'stops.txt')
+
+describe('The GTFS Loaders with the new Tram data', () => {
+  describe('The stop loader', () => {
+    it('Should process the stops and add them to the database', async () => {
+      let database = new LokiDatabaseConnection('test-db')
+      let stops = await database.createCollection('stops')
+
+      let loader = new StopsLoader(stopsFile, suburbs, TRANSIT_MODES.tram, database)
+      await loader.loadStops()
+
+      let stop = await stops.findDocument({
+        'stopName': 'Glenferrie Road/Wattletree Road'
+      })
+
+      expect(stop).to.not.be.null
+      expect(stop.bays[0].stopNumber).to.equal('45')
+    })
+  })
+})
