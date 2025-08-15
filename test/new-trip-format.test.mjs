075b94ff34feac01338fb3b46e770f57cffb28f5
Eyeball Edward <edwardyeung39@gmail.com>
2025-08-15 19:55:00 +1000

    remove metro requirement

--- test/new-trip-format.test.mjs
+++ test/new-trip-format.test.mjs
@@ -8,6 +8,9 @@ const CALENDAR = {
 
 describe('The new trip format class', () => {
   it('Parses Metro trips 02-SUY--11-T2-Z000', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '02-SUY--11-T2-Z000'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '02-SUY--11-T2-Z000'
@@ -15,6 +18,9 @@ describe('The new trip format class', () => {
   })
 
   it('Parses Metro RRB trips 02-STY-R-12-T6-BP436', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '02-STY-R-12-T6-BP436'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '02-STY-R-12-T6-BP436'
@@ -22,6 +28,9 @@ describe('The new trip format class', () => {
   })
 
   it('Parses V/Line trips 01-BDE--10-T2-8460', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '01-BDE--10-T2-8460'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '01-BDE--10-T2-8460'
@@ -29,6 +38,9 @@ describe('The new trip format class', () => {
   })
 
   it('Parses Tram trips 03-1--10-T2-133092762', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '03-1--10-T2-133092762'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '03-1--10-T2-133092762'
@@ -36,6 +48,9 @@ describe('The new trip format class', () => {
   })
 
   it('Parses Coach trips 05-GEL--13-T3-C299', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '05-GEL--13-T3-C299'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '05-GEL--13-T3-C299'
@@ -43,6 +58,9 @@ describe('The new trip format class', () => {
   })
 
   it('Parses Coach trips 05-GEL--3-T0-8807C', () => {
+    expect(IdentifiableTrip.canProcess({
+      id: '05-GEL--3-T0-8807C'
+    })).to.be.true
     expect(new IdentifiableTrip({
       calendar: CALENDAR,
       id: '05-GEL--3-T0-8807C'
